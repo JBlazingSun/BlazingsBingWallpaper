ebe584db5017924575efa2af9205176a50023c33
blazingsss <328801898@qq.com>
2019-04-01 17:32:53 +0800

    add  some assembly

---
+++ BlazingsBingWallpaper/BlazingsBingWallpaper/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazingsBingWallpaper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            testFunc();
+
+        }
+
+        public static void testFunc()
+        {
+            WebClient wc = new WebClient();
+            wc.DownloadFile(new Uri("https://cn.bing.com/th?id=OHR.Uranus_ZH-CN9689723562_1920x1080.jpg&rf=NorthMale_1920x1080.jpg", UriKind.Absolute), "F:\\"+DateTime.Today.ToLongDateString() + ".jpg");
+        }
+    }
+}
---
+++ BlazingsBingWallpaper/BlazingsBingWallpaper/ProgramFunc.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mime;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace BlazingsBingWallpaper
+{
+    public class ProgramFunc
+    {
+        string imageApi = "bing.com/HPImageArchive.aspx?format=js&idx=0&n=1";
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+        //以拉伸方式显示墙纸
+        public void menuItemStretch_Click(object sender, System.EventArgs e)
+        {
+            
+        }
+        public string GetIamgeUrl()
+        {
+            string imageUrl= "https://cn.bing.com"; 
+
+            return imageUrl;
+        }
+
+        public string GetImageApiAddr()
+        {
+            string addr = "";
+
+            return addr;
+        }
+    }
+}
